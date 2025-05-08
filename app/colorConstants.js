1d14aad73d5a4e5bdc79cd4662ee3b7fcdf02472
Shreyansh . <shrey@adobe.com>
2025-05-08 21:01:58 +0530

    break colorCoder file

---
+++ app/colorConstants.js
@@ -0,0 +1,12 @@
+const MajorColorNames = [
+    "WHITE", "RED", "BLACK", "YELLOW", "VIOLET"
+];
+
+const MinorColorNames = [
+    "BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"
+];
+
+module.exports = {
+    MajorColorNames,
+    MinorColorNames
+}; 
